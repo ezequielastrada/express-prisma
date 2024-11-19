d484c14f77e1148cc97f3618c1e4ce302f51dc45
ezequielastrada <eastrada.in@gmail.com>
2024-11-19 02:19:35 -0300

    fix - productRoutes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import categoriesRoutes from './routes/categories.routes.js';
+import productRoutes from './routes/products.routes.js';
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", categoriesRoutes);
+app.use("/api", productRoutes);
 
 app.listen(3000);
 console.log('Server on port', 3000);
